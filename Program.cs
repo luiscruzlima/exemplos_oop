be551dbae2e387cd5a9ef5ed21be033ac8e8db8c
Luis Lima <limaenator@gmail.com>
2020-04-11 15:56:47 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Fatorial
+{
+    class Fatorial
+    {
+        //variaveis
+        public long num, fatorial;
+        //construtor
+        public Fatorial (long numEntrada)
+        {
+            num = numEntrada;
+            fatorial = 1;
+        }
+        //metodo
+        public double Calcular()
+        {
+            while (num > 1)
+            {
+                fatorial = fatorial * num;
+                num = num - 1;
+            }
+            return fatorial;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // obtenção dos dados e construção do objecto
+            Console.Write("Numero: ");
+            Fatorial fatorial = new Fatorial(long.Parse(Console.ReadLine()));
+            //apresentação do resultado
+            Console.WriteLine("Fatorial: " + fatorial.Calcular());
+            Console.ReadLine();
+        }
+    }
+}
